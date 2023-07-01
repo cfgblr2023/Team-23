9f714c150682f78a7643a07708d0d9f5202a156b
Nagaraj Sunagar <nagarajsunagar2002@gmail.com>
2023-07-02 04:30:38 +0530

    updated know more about us

--- src/components/KnowMoreAboutUs.jsx
+++ src/components/KnowMoreAboutUs.jsx
@@ -1,80 +1,104 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import './KnowMoreAboutUs.css';
+
 function KnowMoreAboutUs() {
-  return (
-    <div>
-    <Navbar />
-    <div className='container1'>
-        <img id="images" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
-        <div class="centered">ABOUT US</div>
-    </div>
-    <div className="container2">
-        <div class="col">
-            <div class="row1">
-                <div className='col1'>
-                <img id="smallimages" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
+    return (
+        <div>
+            <Navbar />
+            <div className='container1'>
+                <img
+                    className="main-image"
+                    src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                    alt="charity"
+                />
+                <div className="centered">ABOUT US</div>
             </div>
-            <div className='col1'>
-                <p id="heading1">WE ARE IN A MISSION TO HELP THE HELPNESS</p><br/>
-                <p id='writing'>We are focusing on a free online coaching site for needy and downtrodden students all over India. Today everyone uses a smartphone and it will be easy for a student to learn from a mobile responsive site. Even we are focusing on people who want to learn state languages.We need volunteers to make this free online mobile responsive site go live and uplift many lives from poverty.</p>
-            </div>
-            </div>
-            <div class="row1">
-            <div className='col1'>
-            <p id="heading">Free Study Center</p><br/>
-            <p id='writing'>Underprivileged Students can benefit by taking free tuition on school and college subjects and languages without paying extra money to the paid tuition centers.</p>
-            </div>
-            <div className='col1'>
-                <img id="smallimages" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
-            </div>
-            </div>
-            <div class="row1">
-            <div className='col1'>
-                <img id="smallimages" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
-            </div>
-            <div className='col1'>
-            <p id="heading">Skill Training</p><br/>
-            <p id='writing'>Empowering women to create their businesses by conducting tailoring classes, beauticians, digital marketing, paper bag, and many more, based on their skills and experience.</p>
-            </div>
-            
-            </div>
-            <div class="row1">
-            <div className='col1'>
-            <p id="heading">Online Schooling</p><br/>
-            <p id='writing'>The only idea behind all this is schooling children, whether at home or at school. And, as far as the education need of children is getting fulfilled, there is no issue with homeschooling as well.</p>
-            </div>
-            <div className='col1'>
-                <img id="smallimages" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
-            </div>
-            </div>
-            <div class="row1">
-            <div className='col1'>
-                <img id="smallimages" src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"  alt="charity" />
-            </div>
-            <div className='col1'>
-            <p id="heading">Proper Education</p><br/>
-            <p id='writing'>Education is the best tool to end “poverty”. We want every student in the nation should learn how to catch their fish, instead of begging for a fish. </p>
-            </div> 
-            
-            </div>
-            <div className='row1'>
-            <p id="heading1">ABOUT OUR NGO</p><br/>
-            <p id='writing1'>We make a difference in the lives of people during a very traumatic time in their life. Meeting their Education needs eases their burden.<br/></p>
-            <p id='writing2'>
-            <br/><br/><h5  id='smallheadings'>Straightforwardness:</h5>
-            Lamp Educational and Charitable Trust guarantee total straightforwardness in the entirety of our endeavors and functions with Internal just as outside partners of the association.
-            <br/><h5 id='smallheadings'>Regard:</h5>
-            Lamp Educational and Charitable Trust guarantee regard towards every person and their perspectives and sentiments back to the front of the association despite their cast, belief, gender, or color.
-            <br/><h5 id='smallheadings'>Uprightness:</h5>
-            Lamp Educational and Charitable Trust works with respectability and guarantees the poise of every one of those associated with our undertakings. To agree with all laws, rules, and guidelines presented as a feature of an association.
-            <br/><h5 id='smallheadings'>Compassion:</h5>
-            To be sympathetic to all those related to Lamp Educational and Charitable Trust and guarantee a high feeling of comprehension of the sensations of every one of our partners.
-            </p>
+            <div className="container2">
+                <div className="col">
+                    <div className="row1">
+                        <div className='col1'>
+                            <img
+                                className="small-image"
+                                src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                                alt="charity"
+                            />
+                        </div>
+                        <div className='col1'>
+                            <p className="heading1">WE ARE IN A MISSION TO HELP THE HELPLESS</p>
+                            <p id='writing'>We are focusing on a free online coaching site for needy and downtrodden students all over India. Today, everyone uses a smartphone, and it will be easy for a student to learn from a mobile-responsive site. We also focus on people who want to learn state languages. We need volunteers to make this free online mobile-responsive site go live and uplift many lives from poverty.</p>
+                        </div>
+                    </div>
+                    <div className="row1">
+                        <div className='col1'>
+                            <p className="heading">Free Study Center</p>
+                            <p id='writing'>Underprivileged students can benefit by taking free tuition on school and college subjects and languages without paying extra money to the paid tuition centers.</p>
+                        </div>
+                        <div className='col1'>
+                            <img
+                                className="small-image"
+                                src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                                alt="charity"
+                            />
+                        </div>
+                    </div>
+                    <div className="row1">
+                        <div className='col1'>
+                            <img
+                                className="small-image"
+                                src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                                alt="charity"
+                            />
+                        </div>
+                        <div className='col1'>
+                            <p className="heading">Skill Training</p>
+                            <p id='writing'>Empowering women to create their businesses by conducting tailoring classes, beauticians, digital marketing, paper bag making, and many more, based on their skills and experience.</p>
+                        </div>
+                    </div>
+                    <div className="row1">
+                        <div className='col1'>
+                            <p className="heading">Online Schooling</p>
+                            <p id='writing'>The only idea behind all this is schooling children, whether at home or at school. And as far as the educational needs of children are fulfilled, there is no issue with homeschooling as well.</p>
+                        </div>
+                        <div className='col1'>
+                            <img
+                                className="small-image"
+                                src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                                alt="charity"
+                            />
+                        </div>
+                    </div>
+                    <div className="row1">
+                        <div className='col1'>
+                            <img
+                                className="small-image"
+                                src="https://lampeducationtrust.com/wp-content/uploads/2022/08/aubergine-e1611195670454.jpg"
+                                alt="charity"
+                            />
+                        </div>
+                        <div className='col1'>
+                            <p className="heading">Proper Education</p>
+                            <p id='writing'>Education is the best tool to end poverty. We want every student in the nation to learn how to catch their fish instead of begging for a fish.</p>
+                        </div>
+                    </div>
+                    <div className='row1'>
+                        <p className="heading1">ABOUT OUR NGO</p>
+                        <p id='writing1'>We make a difference in the lives of people during a very traumatic time in their life. Meeting their education needs eases their burden.</p>
+                        <p id='writing2'>
+                            <br /><br /><h5 className='smallheadings'>Straightforwardness:</h5>
+                            Lamp Educational and Charitable Trust guarantees total transparency in all of our endeavors and functions with internal as well as external stakeholders of the organization.
+                            <br /><h5 className='smallheadings'>Respect:</h5>
+                            Lamp Educational and Charitable Trust ensures respect towards every individual and their views and feelings, irrespective of their caste, belief, gender, or color.
+                            <br /><h5 className='smallheadings'>Integrity:</h5>
+                            Lamp Educational and Charitable Trust works with integrity and ensures the dignity of all those involved in our projects. To comply with all laws, rules, and regulations imposed as part of an organization.
+                            <br /><h5 className='smallheadings'>Compassion:</h5>
+                            To be compassionate to all those associated with Lamp Educational and Charitable Trust and ensure a high sense of understanding of the feelings of all our stakeholders.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-  )
+    );
 }
 
-export default KnowMoreAboutUs
+export default KnowMoreAboutUs;
